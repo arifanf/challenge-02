fe516f0f8cf407e09fcdede6fafda1f75a6deb41
arifanf <111201912160@mhs.dinus.ac.id>
2022-03-11 09:41:55 +0700

    create question number 1 to 7 and readme.md

---
+++ 07-hitungTotalPenjualan.js
@@ -0,0 +1,43 @@
+const dataPenjualanPakAldi = [
+  {
+    namaProduct : 'Sepatu Futsal Nike Vapor Academy 8',
+    hargaSatuan: 760000,
+    kategori : "Sepatu Sport",
+    totalTerjual : 90,
+  },
+  {
+    namaProduct : 'Sepatu Warrior Tristan Black Brown High',
+    hargaSatuan: 960000,
+    kategori : "Sepatu Sneaker",
+    totalTerjual : 37,
+  },
+  {
+    namaProduct : 'Sepatu Warrior Tristan Maroon High ',
+    kategori : "Sepatu Sneaker",
+    hargaSatuan: 360000,
+    totalTerjual : 90,
+  },
+  {
+    namaProduct : 'Sepatu Warrior Rainbow Tosca Corduroy',
+    hargaSatuan: 120000,
+    kategori : "Sepatu Sneaker",
+    totalTerjual : 90,
+  }
+]
+
+function hitungTotalPenjualan(dataPenjualan) {
+  
+  if (typeof dataPenjualan === "object") {
+    let jumlahPenjualanTotal = dataPenjualan
+      .map((item) => item.totalTerjual)
+      .reduce((totalSebelum, totalJual) => totalSebelum + totalJual, 0);
+    return jumlahPenjualanTotal;
+  } 
+  else {
+    return "Please input valid data";
+  }
+  
+};
+
+console.log(hitungTotalPenjualan(dataPenjualanPakAldi));
+// EXPECTED OUTPUT => 307
---
+++ README.md
@@ -0,0 +1,5 @@
+# challenge-02
+
+javascript basic algorithm
+
+*Binar X Kampus Merdeka*
